eaef8e57c5a1cff40e7247f18943d017615a6c34
dheeraj_cipherhex <dheeraj.jaiswal@cipherhex.com>
2024-04-08 10:57:59 +0530

    Age calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ageCalculator';
+public  ageCalculator = new FormGroup({
+    day: new FormControl(''),
+    month: new FormControl(''),
+    year: new FormControl(''),
+  });
+
+  public days:any = "--";
+  public months:any = '--';
+  public years:any = '--';
+  calculateAge() {
+    let data= this.ageCalculator.value
+    this.days=data.day;
+    this.months=data.month;
+    this.years=data.year;
+    this.ageCalculator.reset();
+  }
 }
